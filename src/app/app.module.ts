bb55ae8eb2011a7710997e10880104c84ac8bf35
Samsagaz el bravo <WhiteKongSoftware@gmail.com>
2018-10-20 00:41:18 +0200

    header animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 // Pages
 import { HomeComponent } from './pages/home/home.component';
 import { UpsComponent } from './pages/ups/ups.component';
 
 // Components
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -21,6 +23,7 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate, AUTO_STYLE } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('animateState', [
+        state('onTop', style({
+            backgroundColor: 'red'
+        })),
+        state('scrolled', style({
+            backgroundColor: 'yellow',
+            transform: 'translate(-50%, -130px) scale(0.4)',
+        })),
+        transition('* => *', animate(250))
+    ])
+]
 })
 export class HeaderComponent implements OnInit {
+  public state = 'onTop';
+  @HostListener('window:scroll', [])
 
+  onWindowScroll() {
+    if (window.scrollY === 0) {
+      this.state = 'onTop';
+    } else {
+      this.state = 'scrolled';
+
+    }
+  }
   constructor() { }
+
 
   ngOnInit() {
   }
+
 
 }
